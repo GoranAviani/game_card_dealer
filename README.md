3ef2da898f39fbc607675a39574c3e5ab3f516bf
goranaviani <goranaviani@gmail.com>
2019-03-03 18:43:23 +0100

    readme

---
+++ README.md
@@ -0,0 +1,15 @@
+Ok so this weekend there is a bundle on Humble bundle that has TCG cards, icons and
+many other images one could use in building its own trading card game.
+This is my quick idea how to draw cards on the beginning of the game.
+
+<br>
+
+
+
+ This code must have solutions for
+* cards must be shuffled  before the beginning of the draw
+* there must be several globally banned cards that cannot be picked
+* each card that is drawn drawn must have a description
+* after a card is drawn the same card must not be drawn again (ex is removed from the deck)
+* limit a number of cards drawn (ex 6)
+* number of drawn cards can me chosen by the user (6 or 7 cards)
--- gcd.py
+++ gcd.py
@@ -21,7 +21,7 @@ def draw_cards(gameDeck, userNoOfCards): #draw cards will draw as many cards aas
 
 
 def no_of_drawn_cards():
-    canDraw = [5,6,7]
+    canDraw = [5, 6, 7]
     userAnswer = None
     firstTimeAsked = 0
     while userAnswer not in canDraw:
@@ -41,14 +41,14 @@ def no_of_drawn_cards():
 def shuffle_cards(gameDeck): #this def is needed because there might be more shufflinf down the line
     print(gameDeck)
     randomDeck = []
-    for x in range(0,len(gameDeck)):
+    for x in range(0 ,len(gameDeck)):
         pickedNumber = random.choice(gameDeck)
         gameDeck.remove(pickedNumber)
         randomDeck.append(pickedNumber)
     return randomDeck
 
 def get_numbers_of_cards():
-    return [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16] # card no 14 is to strong to play with
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16] # card no 14 is to strong to play with
 
 
 def load_cards():
@@ -95,11 +95,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-# This code must have solutions for this
-# cards must be shuffled  before the beginning of the drawn
-# there must be several banned cards that cannot be picked
-# each card drawn must have a description
-# after a card is drawn the same card must not be drawn again (ex is removed from the deck)
-# limit a number of cards drawn (ex 6)
-# number of drawn cards can me chosed from option menu (6 or 7 cards)
